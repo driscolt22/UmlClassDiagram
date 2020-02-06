f3c574c2d6574c172e192e67749b3248125c99a0
unknown <driscolt@union.edu>
2020-02-06 14:35:38 -0500

    adding a window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,23 @@
 package PACKAGE_NAME;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class Main {
-}
+
+public class Main extends JFrame{
+
+  public Main() {
+
+    this.setVisible(true);
+     setTitle("Simple example");
+     setSize(300, 200);
+     setLocationRelativeTo(null);
+     setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+
+  public static void main(String[] args) {
+     Main ex = new Main();
+
+  }
+
+
+  }
