7cd1f607351f8ec1e6e0ddae61048b8bc624bf23
Jasper Bergh <berghj@union.edu>
2020-02-11 14:58:10 -0500

    Added Head and tail to block

--- src/main/java/app_model/line.java
+++ src/main/java/app_model/line.java
@@ -2,13 +2,15 @@ package app_model;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.util.*;
-
+import app_model.Block;
 
 public class Line implements DisplayObject{
   private int x1;
   private int y1;
   private int x2;
   private int y2;
+  private Block head;
+  private Block tail;
 
 
   public Line(int x1, int y1, int x2, int y2)
@@ -17,6 +19,12 @@ public class Line implements DisplayObject{
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+
+  public Line(Block head, Block tail){
+      this.head = head;
+      this.tail = tail;
+      updatePosition();
   }
 
   public int getFirstX_Value(){
@@ -83,6 +91,19 @@ public class Line implements DisplayObject{
     }
     else
       return false;
+  }
+
+  public void updatePosition(){
+      //sets x1, y1, x2, y2
+      return;
+  }
+
+  public void setHead(){
+      return;
+  }
+
+  public void setTail(){
+      return;
   }
 
   public void accept(Visitor v){
